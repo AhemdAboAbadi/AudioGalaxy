57a2a05c359960ed2392d3284c9836b1513d2f75
AhemdAboAbadi <ahmed.free.py@gmail.com>
2021-07-27 22:58:19 +0300

    creat pop & show data

--- script/logic.js
+++ script/logic.js
@@ -1,1 +1,77 @@
+const body = document.querySelector("body");
+const NasaFunc = (() => {
+    fetch(NasaAPI, (data) => {
+        console.log(data);
+        data.forEach((i) => {
+            //-----------------Creat Elements------------------------
+            const mainFigure = document.createElement("figure");
+            const DivImg = document.createElement("div");
+            const imgD = document.createElement("img");
+            const ContainerContent = document.createElement("div");
+            const FirstLine = document.createElement("div");
+            const H2 = document.createElement("h2");
+            const Btn = document.createElement("button");
+            const SecondLine = document.createElement("div");
+            const ParagraphFavorites = document.createElement("a");
+            const ThirdLine = document.createElement("div");
+            const ParagraphArticle = document.createElement("p");
+            const FourthLine = document.createElement("div");
+            const ParagraphDate = document.createElement("p");
 
+            //---------------Append Childs---------------------------
+
+            mainFigure.appendChild(DivImg);
+            mainFigure.appendChild(ContainerContent);
+            DivImg.appendChild(imgD);
+            ContainerContent.appendChild(FirstLine);
+            ContainerContent.appendChild(SecondLine);
+            ContainerContent.appendChild(ThirdLine);
+            ContainerContent.appendChild(FourthLine);
+            FirstLine.appendChild(H2);
+            FirstLine.appendChild(Btn);
+            SecondLine.appendChild(ParagraphFavorites);
+            ThirdLine.appendChild(ParagraphArticle);
+            FourthLine.appendChild(ParagraphDate);
+
+            body.appendChild(mainFigure);
+
+            //-------------Append Elements---------------------------
+            mainFigure.classList.add("main-contenar-content");
+            DivImg.classList.add("contenar-img");
+            ContainerContent.classList.add("contenar-content");
+            FirstLine.classList.add("first-line")
+            SecondLine.classList.add("second-line")
+            ThirdLine.classList.add("Third-line")
+            Btn.classList.add("volume-btn");
+            H2.classList.add("h2-title");
+            ParagraphFavorites.classList.add("p-add-to-favorites");
+            ParagraphArticle.classList.add("p-article");
+            FourthLine.classList.add("fourth-line")
+            ParagraphDate.classList.add("p-date");
+            ParagraphFavorites.textContent = "Add To Favorites";
+            //--------------Passing values-------------------------
+            if (i.url == undefined || i.url == "") {
+              return;
+          } else {
+              imgD.src = i.url;
+              H2.textContent = i.title;
+              ParagraphArticle.textContent = i.explanation;
+              ParagraphDate.textContent = i.date;
+          }
+        })
+
+    });
+})();
+
+
+const pop_up_to_top = document.querySelector(".pop_up_to_top");
+window.addEventListener("scroll", function () {
+  pop_up_to_top.classList.toggle("active", window.scrollY > 400);
+});
+
+pop_up_to_top.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
