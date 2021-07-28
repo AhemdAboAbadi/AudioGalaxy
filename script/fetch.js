32fc6d48ea3001184d0855c830074e2330c71a51
AhemdAboAbadi <ahmed.free.py@gmail.com>
2021-07-28 10:00:08 +0300

    fixed all files

--- script/fetch.js
+++ script/fetch.js
@@ -1,23 +1,23 @@
-const NasaAPIKey = '29r852l7qXSPuClARF1GnNcuC2aL2ybXHzXS2gOk';
+const NasaAPIKey = "29r852l7qXSPuClARF1GnNcuC2aL2ybXHzXS2gOk";
 const VoicesAPIKey = `b122fe77a30a4863ab2fdd805d455ccf`;
-const startDay = '2020-01-01';
-const endDay = '2021-07-07'
+const startDay = "2020-01-01";
+const endDay = "2021-07-07";
 const NasaAPI = `https://api.nasa.gov/planetary/apod?count=10&api_key=${NasaAPIKey}`;
 // const VoicesAPI = `https://api.voicerss.org/?key=${VoicesAPIKey}&hl=en-us&src=${article}`;
 
 const fetch = (url, cb) => {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            cb(JSON.parse(xhr.responseText));
-        } else if (xhr.status === 404) {
-            alertMess('ERROR');
-        }
-    };
-    xhr.open('GET', url);
-    xhr.send();
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      cb(JSON.parse(xhr.responseText));
+    } else if (xhr.status === 404) {
+      alertMess("ERROR");
+    }
+  };
+  xhr.open("GET", url);
+  xhr.send();
 };
 
-const alertMess = error => {
-    alert('404 Not found');
+const alertMess = (error) => {
+  alert("404 Not found");
 };
--- script/logic.js
+++ script/logic.js
@@ -1,66 +1,77 @@
 const body = document.querySelector("body");
+const container = document.createElement("div");
+body.appendChild(container);
+const loadMore = document.querySelector("#load-anchor");
+
 const NasaFunc = (() => {
-    fetch(NasaAPI, (data) => {
-        console.log(data);
-        data.forEach((i) => {
-            //-----------------Creat Elements------------------------
-            const mainFigure = document.createElement("figure");
-            const DivImg = document.createElement("div");
-            const imgD = document.createElement("img");
-            const ContainerContent = document.createElement("div");
-            const FirstLine = document.createElement("div");
-            const H2 = document.createElement("h2");
-            const Btn = document.createElement("button");
-            const SecondLine = document.createElement("div");
-            const ParagraphFavorites = document.createElement("a");
-            const ThirdLine = document.createElement("div");
-            const ParagraphArticle = document.createElement("p");
-            const FourthLine = document.createElement("div");
-            const ParagraphDate = document.createElement("p");
+  fetch(NasaAPI, (data) => {
+    console.log(data);
+    data.forEach((i) => {
+      //-----------------Creat Elements------------------------
+      const mainFigure = document.createElement("figure");
+      const DivImg = document.createElement("div");
+      const imgD = document.createElement("img");
+      const ContainerContent = document.createElement("div");
+      const FirstLine = document.createElement("div");
+      const H2 = document.createElement("h2");
+      const Btn = document.createElement("button");
+      const SecondLine = document.createElement("div");
+      const ParagraphFavorites = document.createElement("a");
+      const ThirdLine = document.createElement("div");
+      const ParagraphArticle = document.createElement("p");
+      const FourthLine = document.createElement("div");
+      const ParagraphDate = document.createElement("p");
+      const fontAwesome = document.createElement("i");
 
-            //---------------Append Childs---------------------------
+      //---------------Append Childs---------------------------
+      container.appendChild(mainFigure);
+      mainFigure.appendChild(DivImg);
+      mainFigure.appendChild(ContainerContent);
+      DivImg.appendChild(imgD);
+      ContainerContent.appendChild(FirstLine);
+      ContainerContent.appendChild(SecondLine);
+      ContainerContent.appendChild(ThirdLine);
+      ContainerContent.appendChild(FourthLine);
+      FirstLine.appendChild(H2);
+      FirstLine.appendChild(Btn);
+      SecondLine.appendChild(ParagraphFavorites);
+      ThirdLine.appendChild(ParagraphArticle);
+      FourthLine.appendChild(ParagraphDate);
+      Btn.appendChild(fontAwesome);
+      FirstLine.appendChild(Btn)
+      container.appendChild(mainFigure);
 
-            mainFigure.appendChild(DivImg);
-            mainFigure.appendChild(ContainerContent);
-            DivImg.appendChild(imgD);
-            ContainerContent.appendChild(FirstLine);
-            ContainerContent.appendChild(SecondLine);
-            ContainerContent.appendChild(ThirdLine);
-            ContainerContent.appendChild(FourthLine);
-            FirstLine.appendChild(H2);
-            FirstLine.appendChild(Btn);
-            SecondLine.appendChild(ParagraphFavorites);
-            ThirdLine.appendChild(ParagraphArticle);
-            FourthLine.appendChild(ParagraphDate);
+      //-------------Append Elements---------------------------
+      container.classList.add("main-contenar")
+      mainFigure.classList.add("main-contenar-content");
+      imgD.classList.add("imges0");
+      DivImg.classList.add("contenar-img");
+      ContainerContent.classList.add("contenar-content");
+      FirstLine.classList.add("first-line");
+      SecondLine.classList.add("second-line");
+      ThirdLine.classList.add("Third-line");
+      Btn.classList.add("volume-btn");
+      H2.classList.add("h2-title");
+      ParagraphFavorites.classList.add("p-add-to-favorites");
+      ParagraphArticle.classList.add("p-article");
+      FourthLine.classList.add("fourth-line");
+      ParagraphDate.classList.add("p-date");
+      ParagraphFavorites.textContent = "Add To Favorites";
+      fontAwesome.classList.add("fa" ,"fa-volume-up");
+      fontAwesome.setAttribute("aria-hidden", "true");
 
-            body.appendChild(mainFigure);
-
-            //-------------Append Elements---------------------------
-            mainFigure.classList.add("main-contenar-content");
-            DivImg.classList.add("contenar-img");
-            ContainerContent.classList.add("contenar-content");
-            FirstLine.classList.add("first-line")
-            SecondLine.classList.add("second-line")
-            ThirdLine.classList.add("Third-line")
-            Btn.classList.add("volume-btn");
-            H2.classList.add("h2-title");
-            ParagraphFavorites.classList.add("p-add-to-favorites");
-            ParagraphArticle.classList.add("p-article");
-            FourthLine.classList.add("fourth-line")
-            ParagraphDate.classList.add("p-date");
-            ParagraphFavorites.textContent = "Add To Favorites";
-            //--------------Passing values-------------------------
-            if (i.url == undefined || i.url == "") {
-              return;
-          } else {
-              imgD.src = i.url;
-              H2.textContent = i.title;
-              ParagraphArticle.textContent = i.explanation;
-              ParagraphDate.textContent = i.date;
-          }
-        })
-
+      // Btn.createTextNode('<i class="fa fa-volume-up" aria-hidden="true"c></i>')
+      //--------------Passing values-------------------------
+      if (i.url == undefined || i.url == "") {
+        return;
+      } else {
+        imgD.src = i.url;
+        H2.textContent = i.title;
+        ParagraphArticle.textContent = i.explanation;
+        ParagraphDate.textContent = i.date;
+      }
     });
+  });
 })();
 
 
