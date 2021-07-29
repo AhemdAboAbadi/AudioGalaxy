4405f251f0b0f8c4903dc8e7aac4414c3733459e
umar <umars.salah@gmail.com>
2021-07-29 09:36:35 +0300

    Abstract return functions Relates #31

--- script/dom.js
+++ script/dom.js
@@ -2,122 +2,117 @@ const loader = document.querySelector(".loader");
 const timeNow = document.getElementById("timeNow");
 const tts = window.speechSynthesis;
 //Operating functions
-const selector = (select) => {
-  return document.querySelector(select);
-};
+const selector = (select) => document.querySelector(select);
 
-const creatElement = (ele) => {
-  return document.createElement(ele);
-};
+const creatElement = (ele) => document.createElement(ele);
 
-const appendElement = (parent, child) => {
-  return parent.appendChild(child);
-};
+const appendElement = (parent, child) => parent.appendChild(child);
 
-const addClass = (element, className) => {
-  return element.classList.add(className);
-};
+const addClass = (element, className) => element.classList.add(className);
 
 // HTML Elements
 const container = creatElement("div");
 selector("body").appendChild(container);
 
 const createArticle = (i, id) => {
-  container.classList.add("main-contenar");
+    container.classList.add("main-contenar");
 
-  // Card
-  const mainFigure = creatElement("figure");
-  addClass(mainFigure, "main-contenar-content");
-  appendElement(container, mainFigure);
+    // Card
+    const mainFigure = creatElement("figure");
+    addClass(mainFigure, "main-contenar-content");
+    appendElement(container, mainFigure);
 
-  // Image
-  const DivImg = creatElement("div");
-  addClass(DivImg, "contenar-img");
-  const imgD = creatElement("img");
-  addClass(imgD, "imges0");
-  appendElement(DivImg, imgD);
+    // Image
+    const DivImg = creatElement("div");
+    addClass(DivImg, "contenar-img");
+    const imgD = creatElement("img");
+    addClass(imgD, "imges0");
+    appendElement(DivImg, imgD);
 
-  // Things bellow an image
-  const ContainerContent = creatElement("div");
-  addClass(ContainerContent, "contenar-content");
-  const FirstLine = creatElement("div");
-  addClass(FirstLine, "first-line");
-  const H2 = creatElement("h2");
-  H2.classList.add("h2-title");
+    // Things bellow an image
+    const ContainerContent = creatElement("div");
+    addClass(ContainerContent, "contenar-content");
+    const FirstLine = creatElement("div");
+    addClass(FirstLine, "first-line");
+    const H2 = creatElement("h2");
+    H2.classList.add("h2-title");
 
-  //Sound Button
-  const Btn = creatElement("button");
-  const fontAwesome = creatElement("i");
-  Btn.classList.add("volume-btn");
-  Btn.setAttribute("id", `${id}`);
-  addClass(fontAwesome, "fa");
-  addClass(fontAwesome, "fa-volume-up");
-  fontAwesome.setAttribute("aria-hidden", "true");
-  appendElement(Btn, fontAwesome);
-  appendElement(FirstLine, Btn);
-  Btn.addEventListener("click", (e) => {
-    fetch(`https://api.nasa.gov/planetary/apod?count=10&api_key=29r852l7qXSPuClARF1GnNcuC2aL2ybXHzXS2gOk`, (data) => {
-      window.speechSynthesis.cancel();
-      const text = data[id].explanation;
-      const toSpeack = new SpeechSynthesisUtterance(`${text}`);
-      const voice = tts.getVoices()[1];
-      toSpeack.voice = voice;
-      tts.speak(toSpeack);
-    })
-  })
+    //Sound Button
+    const Btn = creatElement("button");
+    const fontAwesome = creatElement("i");
+    Btn.classList.add("volume-btn");
+    Btn.setAttribute("id", `${id}`);
+    addClass(fontAwesome, "fa");
+    addClass(fontAwesome, "fa-volume-up");
+    fontAwesome.setAttribute("aria-hidden", "true");
+    appendElement(Btn, fontAwesome);
+    appendElement(FirstLine, Btn);
+    Btn.addEventListener("click", (e) => {
+        fetch(
+            `https://api.nasa.gov/planetary/apod?count=10&api_key=29r852l7qXSPuClARF1GnNcuC2aL2ybXHzXS2gOk`,
+            (data) => {
+                window.speechSynthesis.cancel();
+                const text = data[id].explanation;
+                const toSpeack = new SpeechSynthesisUtterance(`${text}`);
+                const voice = tts.getVoices()[1];
+                toSpeack.voice = voice;
+                tts.speak(toSpeack);
+            }
+        );
+    });
 
-  //Button add favorites
-  const SecondLine = creatElement("div");
-  addClass(SecondLine, "second-line");
-  const ParagraphFavorites = creatElement("button");
-  addClass(ParagraphFavorites, "p-add-to-favorites");
-  ParagraphFavorites.textContent = "Add To Favorites";
-  appendElement(SecondLine, ParagraphFavorites);
+    //Button add favorites
+    const SecondLine = creatElement("div");
+    addClass(SecondLine, "second-line");
+    const ParagraphFavorites = creatElement("a");
+    addClass(ParagraphFavorites, "p-add-to-favorites");
+    ParagraphFavorites.textContent = "Add To Favorites";
+    appendElement(SecondLine, ParagraphFavorites);
 
-  //Article
-  const ThirdLine = creatElement("div");
-  addClass(ThirdLine, "Third-line");
-  const ParagraphArticle = creatElement("p");
-  addClass(ParagraphArticle, "p-article");
-  appendElement(ThirdLine, ParagraphArticle);
+    //Article
+    const ThirdLine = creatElement("div");
+    addClass(ThirdLine, "Third-line");
+    const ParagraphArticle = creatElement("p");
+    addClass(ParagraphArticle, "p-article");
+    appendElement(ThirdLine, ParagraphArticle);
 
-  //Date
-  const FourthLine = creatElement("div");
-  addClass(FourthLine, "fourth-line");
-  const ParagraphDate = creatElement("p");
-  addClass(ParagraphDate, "p-date");
-  appendElement(FourthLine, ParagraphDate);
+    //Date
+    const FourthLine = creatElement("div");
+    addClass(FourthLine, "fourth-line");
+    const ParagraphDate = creatElement("p");
+    addClass(ParagraphDate, "p-date");
+    appendElement(FourthLine, ParagraphDate);
 
-  mainFigure.append(DivImg, ContainerContent);
-  ContainerContent.append(FirstLine, SecondLine, ThirdLine, FourthLine);
-  FirstLine.append(H2, Btn);
+    mainFigure.append(DivImg, ContainerContent);
+    ContainerContent.append(FirstLine, SecondLine, ThirdLine, FourthLine);
+    FirstLine.append(H2, Btn);
 
-  const unique = i.url;
-  if (unique == undefined || unique == "" || i.media_type == "video") {
-    return;
-  } else {
-    imgD.src = i.url;
-    H2.textContent = i.title;
-    ParagraphArticle.textContent = i.explanation;
-    ParagraphDate.textContent = i.date;
-  }
+    const unique = i.url;
+    if (unique == undefined || unique == "" || i.media_type == "video") {
+        return;
+    } else {
+        imgD.src = i.url;
+        H2.textContent = i.title;
+        ParagraphArticle.textContent = i.explanation;
+        ParagraphDate.textContent = i.date;
+    }
 };
 const clearContainer = () => {
-  while (container.firstChild) {
-    container.removeChild(container.firstChild);
-    loader.classList.remove("hidden");
-  }
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+        loader.classList.remove("hidden");
+    }
 };
 // Pop up
 const pop_up_to_top = document.querySelector(".pop_up_to_top");
-window.addEventListener("scroll", function () {
-  pop_up_to_top.classList.toggle("active", window.scrollY > 400);
+window.addEventListener("scroll", function() {
+    pop_up_to_top.classList.toggle("active", window.scrollY > 400);
 });
 
-pop_up_to_top.addEventListener("click", function () {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
+pop_up_to_top.addEventListener("click", function() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
 });
 timeNow.textContent = greetings(new Date().getHours());
